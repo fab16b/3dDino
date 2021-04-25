c4115af09ead2526432843943e6e6ca118521523
Felix Mbikogbia <fab16b@acu.edu>
2021-04-25 14:37:26 -0500

    Initial Commit

---
+++ 3dDino/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3dDino
+//
+//  Created by Felix Mbikogbia  on 4/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
